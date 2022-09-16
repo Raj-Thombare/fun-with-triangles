9e87bad49bf93e960f54644aff57f1195a08b676
Raj Thombare <therajthombare@gmail.com>
2022-09-16 20:07:39 +0530

    fix: area inputs accepting negative values

--- area.js
+++ area.js
@@ -7,7 +7,16 @@ function areaIs(){
     const base = Number(b.value);
     const height = Number(h.value);
     const area = (base * height)/2;
-    output.innerText = `The area of triangle is ${area} cm`;
+    
+    if(area && height){
+       if(area > 0 & height > 0){
+          output.innerText = `The area of triangle is ${area} cm`;
+       }else{
+        output.innerText = "Base and Height of triangle should be positive";
+       }
+    }else{
+     output.innerText = "Please enter base and height of triangle!";
+    }
 }
 
 btnCalculate.addEventListener("click", areaIs)
